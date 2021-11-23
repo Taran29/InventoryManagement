a918db56c05f8ba7d6ef1ac636792d06fe6da0b0
Taranpreet <taranpreetsingh29@gmail.com>
2021-11-23 11:45:37 +0530

    User Model created

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
+const Joi = require('joi')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -20,3 +22,24 @@ const userSchema = new mongoose.Schema({
     default: 'Employee'
   },
 })
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign( { _id: this._id }, process.env.jwtPrivateKey)
+  return token
+}
+
+const User = mongoose.model('user', userSchema)
+
+const validateUser = (user) => {
+  const schema = new Joi.object({
+    name: Joi.string().required().trim().max(50),
+    email: Joi.string().email().required().trim().max(255),
+    password: Joi.string().min(8).max(24).required(),
+    roles: Joi.string().valid('Admin', 'Employee').default('Employee')
+  })
+
+  return schema.validate(user)
+}
+
+module.exports.User = User
+module.exports.validateUser = validateUser
